3f1b92b45315ce03980d5fb230a5121733d7c533
Josh Kabo <joshkong1@gmail.com>
2023-04-27 13:48:49 -0700

    roughly working on hardcoded set

--- TextPalette.py
+++ TextPalette.py
@@ -3,41 +3,42 @@ import pyperclip
 import pickle
 import tkinter as tk
 
-paste_dict = {"Test": "This is a test"}
+paste_dict = {}
 
 # gives the key for c
 
 
-def copyCallback(key):
-    print("copy callback called with %s as the key!" % key)
-    pass
-
-
 def main():
-    # print("Hopefully the clipboard should now have copy thing in it in it")
-    # pyperclip.copy("this is the copy thing")
+    paste_dict = load_paste_dict()
+
     window = tk.Tk()
+
+    # this window will have high priority, draw on top
+    window.attributes('-topmost', True)
+
     greeting = tk.Label(text="Hello Tkinter")
     greeting.pack()
-    # button = tk.Button(
-    #     text=
-    # )
-    for i in range(0, 5):
-        button = tk.Button(master=window, text='button %d' % i,
-                           command=lambda i=i: copyCallback("press keeyy %d" % i))
+    # NOTE: regen buttons after an update happens (that way all the callbacks will behave the way I want them to.)
+    for key in paste_dict.keys():
+        button = tk.Button(master=window, text=key,
+                           command=lambda key=key: pyperclip.copy(paste_dict[key]))
         button.pack()
 
     window.mainloop()
 
-# # write the dict out to a file
-# def save_paste_dict():
-#     with open("paste_dict.pkl", "wb") as pickled_dict_file:
-#         pickle.dump(paste_dict, pickled_dict_file)
+# write the dict out to a file
+
+
+def save_paste_dict(to_save):
+    with open("paste_dict.pkl", "wb") as pickled_dict_file:
+        pickle.dump(to_save, pickled_dict_file)
 
 # # read the dict from the file
-# def load_paste_dict():
-#     with open("paste_dict.pkl", "rb") as pickled_dict_file:
-#         paste_dict = pickle.load(pickled_dict_file)
+
+
+def load_paste_dict():
+    with open("paste_dict.pkl", "rb") as pickled_dict_file:
+        return pickle.load(pickled_dict_file)
 
 
 if __name__ == "__main__":
